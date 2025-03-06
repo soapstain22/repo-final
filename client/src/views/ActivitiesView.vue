5a68ce4ea3fba83c5e1597e6b4795dc2c4cba828
superpizza <36314018+soapstain22@users.noreply.github.com>
2025-03-06 09:35:31 -0500

    new placeholder

--- client/src/views/ActivitiesView.vue
+++ client/src/views/ActivitiesView.vue
@@ -1,43 +1,17 @@
 <template>
   <div class="container">
     <h1 class="title">Activities</h1>
-    <table class="table is-fullwidth">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Date</th>
-          <th>Duration</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Running</td>
-          <td>2024-01-01</td>
-          <td>30 minutes</td>
-          <td>
-            <button class="button is-small is-info">Edit</button>
-            <button class="button is-small is-danger">Delete</button>
-          </td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Swimming</td>
-          <td>2024-01-02</td>
-          <td>45 minutes</td>
-          <td>
-            <button class="button is-small is-info">Edit</button>
-            <button class="button is-small is-danger">Delete</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <ActivityList :activities="activities" />
     <button class="button is-primary">Add Activity</button>
   </div>
 </template>
 
 <script setup lang="ts">
+import ActivityList from '../components/ActivityList.vue';
+import { ref } from 'vue';
 
+const activities = ref([
+  { id: 1, name: 'Running', date: '2024-01-01', duration: '30 minutes' },
+  { id: 2, name: 'Swimming', date: '2024-01-02', duration: '45 minutes' },
+]);
 </script>
